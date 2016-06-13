495ad096fa754bb50694811a285bdcf6f2e551c0
Joseph.Maher <joseph-maher@hotmail.co.uk>
2016-06-13 16:14:06 +0100

    initial commit for homeword w2d1

---
+++ week_2/day_1/ruby_classes_homework/answers/specs/bear_spec.rb
@@ -0,0 +1,6 @@
+require('minitest/autorun')
+require_relative('../bears')
+
+class TestBears < MiniTest::Test
+  
+end
---
+++ week_2/day_1/ruby_classes_homework/answers/specs/river_spec.rb
@@ -0,0 +1,6 @@
+require('minitest/autorun')
+require_relative('../river')
+
+class TestRiver < MiniTest::Test
+  
+end
